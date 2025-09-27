311d56be2d4b4f38a9b26291896b28f9b63ae0d4
ryanmio <ryan@mioduski.us>
2025-09-27 15:06:20 -0400

    Make case detail page fully dynamic to avoid stale SSR props on prod
    - force-dynamic + revalidate 0 + force-no-store

--- web/src/app/cases/[id]/page.tsx
+++ web/src/app/cases/[id]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 import Link from "next/link";
 import { LiveViolations, LiveSender, LiveSummary, RequestDeletionButton, CommentsSection, InboundSMSViewer, EvidenceTabs } from "./client";
 import LocalTime from "@/components/LocalTime";
